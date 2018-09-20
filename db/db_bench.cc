a6d760ca554bdae58983838bf1619b9a9d5f74d4
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2018-09-20 14:36:08 +0000

    db_bench changes in rangequery config

--- db/db_bench.cc
+++ db/db_bench.cc
@@ -568,7 +568,7 @@ class Benchmark {
       } else if (name == Slice("readrandom")) {
         method = &Benchmark::ReadRandom;
       } else if (name == Slice("rangequery")) {
-        ranges_ = FLAGS_num / FLAGS_range_size;
+        ranges_ = FLAGS_num / 5;
         range_size_ = FLAGS_range_size;
         method = &Benchmark::RangeQuery;
       } else if (name == Slice("readmissing")) {
